38dbffd43ba0114287d87ed9a51e71f3a4baf86c
JhonnyJr <38056461+JhonnyJr@users.noreply.github.com>
2018-06-23 15:05:44 -0300

    Update juroscompostos.js

--- juroscompostos.js
+++ juroscompostos.js
@@ -3,8 +3,8 @@ window.onload = init;
 var button
 
 function init() {
-	button=document.getElementById("botao");
-	button.addEventListener('click', calcular)
+	button=document.getElementById("botao"); // Atribuindo button ao elemento "botao" em HTML
+	button.addEventListener('click', calcular) // Fazendo com que ao clicar, "puxe" a função calcular()
 }
 
 function calcular() {
@@ -15,15 +15,11 @@ function calcular() {
 	var i // Índice
 	var M // Resultado final
 	JA = JA / 100; // Passando para porcentagem
-	//JA = JA + 1;
-	//JA = Math.pow(JA, T);
-	//C = C * JA;
-
 
 	for(i=1;i<=T;i++){ // "for" Para calcular os juros determinado pela quantidade de meses
 		console.log(12)
 		M= CI*(1+JA);
-		document.getElementById("R").innerHTML += i  + "--" + CI + "--" + M + '<br>';
+		document.getElementById("R").innerHTML += i  + "--" + CI + "--" + M + '<br>'; // Mostrar o resultado 
 		CI = M;
 	}
 }
